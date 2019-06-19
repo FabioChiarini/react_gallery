d1d3213f44aa3dc3f713092b74861b868ae895ce
Fabio Chiarini <chiarrss@gmail.com>
2019-06-19 06:36:21 +0200

    Link in pre set anchor tag

--- src/Nav.js
+++ src/Nav.js
@@ -1,26 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = props => {
   return (
     <nav className="main-nav">
-    <ul>
-      <li>
-        <a href="#" onClick={() => props.getImages("cat")}>
-          Cats
-        </a>
-      </li>
-      <li>
-        <a href="#" onClick={() => props.getImages("dog")}>
-          Dogs
-        </a>
-      </li>
-      <li>
-        <a href="#" onClick={() => props.getImages("computer")}>
-          Computers
-        </a>
-      </li>
-    </ul>
-  </nav>
+      <ul>
+        <li>
+          <Link to="/cats" onClick={() => props.getImages("cat")}>
+            Cats
+          </Link>
+        </li>
+        <li>
+          <Link to="/dogs" onClick={() => props.getImages("dog")}>
+            Dogs
+          </Link>
+        </li>
+        <li>
+          <Link to="/computers" onClick={() => props.getImages("computer")}>
+            Computers
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
