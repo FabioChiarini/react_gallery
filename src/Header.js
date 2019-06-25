4f9f572a9576a492230845dca1f35a77f5e32bb7
Fabio <chiarrss@gmail.com>
2019-06-25 07:50:45 +0200

    Removed some console log

--- src/Header.js
+++ src/Header.js
@@ -3,15 +3,14 @@ import Nav from "./Nav";
 import SearchForm from "./SearchForm";
 
 const Header = props => {
-  console.log(props);
   return (
     <div>
       {/* Adding Search form component to the page and passing the 
         getImage function as a prop, so that it can be used 
         in the SearchForm component */}
-      <SearchForm getImages={props.getImages} />
+      <SearchForm {...props} getImages={props.getImages} />
       {/* Adding the Nav components with his anchor tags */}
-      <Nav getImages={props.getImages} />
+      <Nav {...props} getImages={props.getImages} />
     </div>
   );
 };
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -17,7 +17,6 @@ class SearchForm extends Component {
     let path = `/search/${this.state.value}`;
     this.props.history.push(path, this.props.getImages(this.state.value));
     this.props.getImages(this.state.value);
-    console.log(this.props.history);
     e.currentTarget.reset();
   };
 
